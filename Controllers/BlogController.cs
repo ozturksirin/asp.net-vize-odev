556711581582559e7cd7e7d7b535928dd2c2afae
ozturk sirin <ozturk2002.zri@gmail.com>
2022-11-19 23:48:39 +0300

    finish

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanzHow.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult newScreen()
+        {
+            return View();
+        }
+
+    }
+}
